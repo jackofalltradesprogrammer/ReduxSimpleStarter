8f397805b40f5259b931b61920d2216ed10a9463
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-12-31 17:03:21 -0500

    Add the first component

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Create a new component. This component should produce
+// some html
+const App = function() {    // const is declaring a variable but it's constant and is never going to change
+    return <div>Hi!</div>;  // stuff that looks like html
+}
+
+// Take this component's generated HTML and put it
+// on the page (in the DOM)
