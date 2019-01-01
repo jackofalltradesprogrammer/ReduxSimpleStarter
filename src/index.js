1843d0569faabf7757fd3feadc3df04e5ebaedeb
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-12-31 20:42:17 -0500

    Create a component and add it DOM

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import React from 'react'; // Need React libary to create and manage our components
+import ReactDOM from 'react-dom'; // To interact with actual DOM
+
 // Create a new component. This component should produce
 // some html
-const App = function() {    // const is declaring a variable but it's constant and is never going to change
+const App = () => {    // const is declaring a variable but it's constant and is never going to change
     return <div>Hi!</div>;  // stuff that looks like html
 }
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
