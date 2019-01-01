d04e31fc091d4bd8ff3b785894b62db2ef28e25c
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-01 10:51:34 -0500

    Search bar component and import into index.js

---
+++ src/components/search_bar.js
@@ -0,0 +1,7 @@
+import React from 'react'; // we need React because JSX uses react to render
+
+const SearchBar = () => {
+    return <input />
+};
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react'; // Need React libary to create and manage our components
 import ReactDOM from 'react-dom'; // To interact with actual DOM
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyCa0N0I9Fb0wkUURLwSSaqV2FyidTKfF8s'; // to make request to Youtube
 
 // Create a new component. This component should produce 
 // some html. A component is a function or an object that returns some amount of html
 const App = () => {    // const is declaring a variable but it's constant and is never going to change
-    return <div>Hi!</div>;  // stuff that looks like html
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );  // stuff that looks like html
 }
 
 // Take this component's generated HTML and put it
