0fbe616cd7c1d3194bc8f80db1f397b0220c32ed
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-01 15:06:33 -0500

    Refactoring from functional to class comp to have state of the object

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
-import React from 'react'; // Need React libary to create and manage our components
+import React, {Component} from 'react'; // Need React libary to create and manage our components
 import ReactDOM from 'react-dom'; // To interact with actual DOM
 import YTSearch  from 'youtube-api-search'; // libarary to search item 
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyCa0N0I9Fb0wkUURLwSSaqV2FyidTKfF8s'; // to make request to Youtube
-
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data);
-});
 
 // Create a new component. This component should produce 
 // some html. A Functional component is a function or an object that returns some amount of html
-const App = () => {    // const is declaring a variable but it's constant and is never going to change
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );  // JSX stuff that looks like html with Babel & WebPack
+// we need a class component so it  can record the state of an object
+class App extends Component {    // When app boots up, Constructor will be loaded first 
+    constructor(props)  {
+        super(props);
+         
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({videos}); // in ES6 it  translates to "this.setState({videos: videos});" Only works with same field names
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );  // JSX stuff that looks like html with Babel & WebPack
+    }
 }
 
 // Take this component's generated HTML and put it
