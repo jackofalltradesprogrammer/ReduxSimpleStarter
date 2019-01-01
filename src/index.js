a3a2d580fee56b7a5184c537f5100f7acaaec5ac
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-01 14:54:13 -0500

    Making an api call to youtube

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react'; // Need React libary to create and manage our components
 import ReactDOM from 'react-dom'; // To interact with actual DOM
-
+import YTSearch  from 'youtube-api-search'; // libarary to search item 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCa0N0I9Fb0wkUURLwSSaqV2FyidTKfF8s'; // to make request to Youtube
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+    console.log(data);
+});
 
 // Create a new component. This component should produce 
 // some html. A Functional component is a function or an object that returns some amount of html
