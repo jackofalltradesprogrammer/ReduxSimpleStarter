b781f06b5cfe6e4cf3eca331d500bdbc2d765b2f
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-01 15:51:52 -0500

    use map property to loop over the videos

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 // Whenever REACT dom is rendered, the props are passed as arguments
 // for a class component "props" are available as "this.props"
 const VideoList = (props) => {
+    const videoItems = props.videos.map((video) => {
+        // Unique key "etag" for each list item so React can render it in an efficient way
+        return <VideoListItem key={video.etag} video={video} /> 
+    });
     return  (
         <ul className="col-md-4 list-group">
-        {props.videos.length}
+            {videoItems} {/*  React will try to render this array of components */}
         </ul>
     );
 };
