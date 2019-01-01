9935568a344708e88ab8c66c6e225d2d7a59fe39
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-01 09:08:08 -0500

    Setup component structure to create a YOUTube like page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'; // Need React libary to create and manage our components
 import ReactDOM from 'react-dom'; // To interact with actual DOM
 
-// Create a new component. This component should produce
-// some html
+// Create a new component. This component should produce 
+// some html. A component is a function or an object that returns some amount of html
 const App = () => {    // const is declaring a variable but it's constant and is never going to change
     return <div>Hi!</div>;  // stuff that looks like html
 }
